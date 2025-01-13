e4424002f254fe749c11adb5aec82ed02da979d4
SamuelChauche <samuel.chauche@gmail.com>
2025-01-13 22:41:28 +0100

    Third commit exercice 10

--- exo_10.rb
+++ exo_10.rb
@@ -1,2 +1,13 @@
 #Écris un programme exo_10.rb qui demande son année de naissance à l'utilisateur et qui va afficher chaque année depuis son année de naissance jusqu'aujourd'hui. 
 #Pour chaque année affichée, le programme devra annoncer l'âge que l'utilisateur avait cette année-là.
+#
+puts "Veuillez entrer votre année de naissance :"
+number = gets.to_i
+
+current_year = 2025
+age = 0
+(number..current_year).each do |year|
+  puts "En #{year}, vous aviez #{age} ans."
+  age += 1
+end
+
