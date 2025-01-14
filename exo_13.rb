8c6645727e6de978a77a6df409017804bd371668
SamuelChauche <samuel.chauche@gmail.com>
2025-01-14 15:59:39 +0100

    last commit - derniere modif

--- exo_13.rb
+++ exo_13.rb
@@ -21,16 +21,16 @@
 #afficher(faux_emails)
 #Fin
 
-prenoms = ["alice", "julien", "samuel", "maxime", "bertrand", "clitorine", "marie", "julie", "julia", "julien"]
-noms = ["smith", "johnson", "williams", "brown", "jones", "garcia", "miller", "davis", "rodriguez", "martinez"]
-nom_de_domaine = ["@gmail.com", "@yahoo.com", "@hotmail.com", "@outlook.com", "@orange.fr", "@sfr.fr", "@free.fr", "@laposte.net", "@wanadoo.fr", "@bbox.fr"]
+prenoms = "jean"
+noms = "Dupont"
+nom_de_domaine = "@email.fr"
 counter = 1
 faux_emails=[]
 
 50.times do 
-	Psample= prenoms.sample
-	Nsample= noms.sample
-	Dsample= nom_de_domaine.sample
+	Psample= prenoms
+	Nsample= noms
+	Dsample= nom_de_domaine
 	email = "#{Psample}.#{Nsample}#{counter}#{Dsample}"
 	
 		faux_emails << email
--- exo_14.rb
+++ exo_14.rb
@@ -1,13 +1,13 @@
-prenoms = ["alice", "julien", "samuel", "maxime", "bertrand", "clitorine", "marie", "julie", "julia", "julien"]
-noms = ["smith", "johnson", "williams", "brown", "jones", "garcia", "miller", "davis", "rodriguez", "martinez"]
-nom_de_domaine = ["@gmail.com", "@yahoo.com", "@hotmail.com", "@outlook.com", "@orange.fr", "@sfr.fr", "@free.fr", "@laposte.net", "@wanadoo.fr", "@bbox.fr"]
+prenoms = "jean"
+noms = "Dupont"
+nom_de_domaine = "@email.fr"
 counter = 1
 faux_emails=[]
 
 50.times do 
-	Psample= prenoms.sample
-	Nsample= noms.sample
-	Dsample= nom_de_domaine.sample
+	Psample= prenoms
+	Nsample= noms
+	Dsample= nom_de_domaine
 
 	if counter.even?
 		email = "#{Psample}.#{Nsample}#{counter}#{Dsample}"
